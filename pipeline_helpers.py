859945f25f858a5b1ca808b92b239876ef743d28
Ahmed Tammaa <ahmedtammaa101@hotmail.com>
2023-12-20 10:11:47 +0200

    Update pipeline_helpers.py

--- pipeline_helpers.py
+++ pipeline_helpers.py
@@ -15,7 +15,7 @@ class ExtractAndPredict(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         # nltk.download('punkt')
-        st.write(X)
+        st.write(type(X))
         text = utils.process_folder(X)
         # Concatenate text into one long string
         text = "".join(text.values())
